46103179902b99352b6361b690f347354813a67c
Aaron Lockhart <aaronlockhartdev@gmail.com>
2019-11-08 14:54:31 -0500

    added progress bar

--- get_data.py
+++ get_data.py
@@ -20,38 +20,38 @@ def get_data():
 
     count = 0
 
+        def face_extract(vidPath, map):
+
+            global count
+
+            faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+            video = cv2.VideoCapture(vidPath)
+
+            count = 0
+            success = 1
+
+            while success:
+                success, img = video.read()
+
+                faces = faceCascade.detectMultiScale(img, 1.1, 4)
+
+                if len(faces) == 0:
+                    continue
+
+                (x, y, w, h) = faces[0]
+                img = img[y:y+h, x:x+w]
+
+                img = cv2.resize(img, (IMAGE_WIDTH, IMAGE_HEIGHT))
+
+                map[count] = img
+
+                count += 1
+
     for i in range(len(files)):
         face_extract(DATA_INPUT_PATH + '/' + files[i], map)
         bar.next()
 
     np.save(DATA_OUTPUT_PATH + 'images.npy', np.asarray(dataList))
-
-    def face_extract(vidPath, map):
-
-        global count
-
-        faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-        video = cv2.VideoCapture(vidPath)
-
-        count = 0
-        success = 1
-
-        while success:
-            success, img = video.read()
-
-            faces = faceCascade.detectMultiScale(img, 1.1, 4)
-
-            if len(faces) == 0:
-                continue
-
-            (x, y, w, h) = faces[0]
-            img = img[y:y+h, x:x+w]
-
-            img = cv2.resize(img, (IMAGE_WIDTH, IMAGE_HEIGHT))
-
-            map[count] = img
-
-            count += 1
 
 
 if __name__ == '__main__':
