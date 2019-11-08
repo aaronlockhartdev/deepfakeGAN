078f33b6a7fc73f0ff818ecc9cb7203e4c6467d3
Aaron Lockhart <aaronlockhartdev@gmail.com>
2019-11-08 14:53:35 -0500

    implemented memory map to save RAM

--- get_data.py
+++ get_data.py
@@ -13,40 +13,46 @@ def get_data():
 
     files = os.listdir(DATA_INPUT_PATH)
 
-    dataList = []
-
+    map = np.memmap('data.map', dtype=np.float32,
+                    mode='w+', shape=(200000, 3, 100, 100))
 
     bar = IncrementalBar('Extracting', max=len(files))
+
+    count = 0
+
     for i in range(len(files)):
-        face_extract(DATA_INPUT_PATH + '/' + files[i], dataList)
+        face_extract(DATA_INPUT_PATH + '/' + files[i], map)
         bar.next()
 
     np.save(DATA_OUTPUT_PATH + 'images.npy', np.asarray(dataList))
 
+    def face_extract(vidPath, map):
 
+        global count
 
-def face_extract(vidPath, dataList):
+        faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        video = cv2.VideoCapture(vidPath)
 
-    faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    video = cv2.VideoCapture(vidPath)
+        count = 0
+        success = 1
 
-    count = 0
-    success = 1
+        while success:
+            success, img = video.read()
 
-    while success:
-        success, img = video.read()
+            faces = faceCascade.detectMultiScale(img, 1.1, 4)
 
-        faces = faceCascade.detectMultiScale(img, 1.1, 4)
+            if len(faces) == 0:
+                continue
 
-        if len(faces) == 0:
-            continue
+            (x, y, w, h) = faces[0]
+            img = img[y:y+h, x:x+w]
 
-        (x, y, w, h) = faces[0]
-        img = img[y:y+h, x:x+w]
+            img = cv2.resize(img, (IMAGE_WIDTH, IMAGE_HEIGHT))
 
-        img = cv2.resize(img, (IMAGE_WIDTH, IMAGE_HEIGHT))
+            map[count] = img
 
-        dataList.append(np.copy(img))
+            count++
+
 
 if __name__ == '__main__':
      get_data()
