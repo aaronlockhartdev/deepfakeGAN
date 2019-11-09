d7ebcc6a3ed159d7b382c32320667bc7647db49f
BuildTools <unconfigured@null.spigotmc.org>
2019-11-09 00:23:18 -0500

    updated .gitignore

--- constants.py
+++ constants.py
@@ -7,6 +7,6 @@ DATA_OUTPUT_PATH = "X:/output_faceforensics"
 DATA_INPUT_PATH = "/share/faceforensics/original_sequences/youtube/raw/videos"
 
 # training
-BATCH_SIZE = 2
+BATCH_SIZE = 16
 EPOCHS = 1000
 CHECKPOINT_DIR = "checkpoints"
